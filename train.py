6a7a32aa4f48703cd6ea4f48e5c587b84d979b2a
Osama Mirza <99750385+0sama-Mirza@users.noreply.github.com>
2024-12-02 16:53:18 +0000

    Update train.py

--- train.py
+++ train.py
@@ -12,6 +12,10 @@ import time
 import datetime
 import math
 from models.retinaface import RetinaFace
+from torch.cuda.amp import autocast, GradScaler  # Import for mixed precision
+
+# Set memory management environment variable for avoiding fragmentation
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 
 parser = argparse.ArgumentParser(description='Retinaface Training')
 parser.add_argument('--training_dataset', default='./data/widerface/train/label.txt', help='Training dataset directory')
@@ -77,7 +81,6 @@ else:
 
 cudnn.benchmark = True
 
-
 optimizer = optim.SGD(net.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
 criterion = MultiBoxLoss(num_classes, 0.35, True, 0, True, 7, 0.35, False)
 
@@ -86,12 +89,15 @@ with torch.no_grad():
     priors = priorbox.forward()
     priors = priors.cuda()
 
+# Initialize mixed precision scaler
+scaler = GradScaler()
+
 def train():
     net.train()
     epoch = 0 + args.resume_epoch
     print('Loading Dataset...')
 
-    dataset = WiderFaceDetection( training_dataset,preproc(img_dim, rgb_mean))
+    dataset = WiderFaceDetection(training_dataset, preproc(img_dim, rgb_mean))
 
     epoch_size = math.ceil(len(dataset) / batch_size)
     max_iter = max_epoch * epoch_size
@@ -109,7 +115,7 @@ def train():
             # create batch iterator
             batch_iterator = iter(data.DataLoader(dataset, batch_size, shuffle=True, num_workers=num_workers, collate_fn=detection_collate))
             if (epoch % 10 == 0 and epoch > 0) or (epoch % 5 == 0 and epoch > cfg['decay1']):
-                torch.save(net.state_dict(), save_folder + cfg['name']+ '_epoch_' + str(epoch) + '.pth')
+                torch.save(net.state_dict(), save_folder + cfg['name'] + '_epoch_' + str(epoch) + '.pth')
             epoch += 1
 
         load_t0 = time.time()
@@ -123,20 +129,25 @@ def train():
         targets = [anno.cuda() for anno in targets]
 
         # forward
-        out = net(images)
+        with autocast():  # Enable mixed precision
+            out = net(images)
 
         # backprop
         optimizer.zero_grad()
         loss_l, loss_c, loss_landm = criterion(out, priors, targets)
         loss = cfg['loc_weight'] * loss_l + loss_c + loss_landm
-        loss.backward()
-        optimizer.step()
+        scaler.scale(loss).backward()  # Use scaler for mixed precision
+        scaler.step(optimizer)
+        scaler.update()
+
+        torch.cuda.empty_cache()  # Clear memory cache after each batch
+
         load_t1 = time.time()
         batch_time = load_t1 - load_t0
         eta = int(batch_time * (max_iter - iteration))
         print('Epoch:{}/{} || Epochiter: {}/{} || Iter: {}/{} || Loc: {:.4f} Cla: {:.4f} Landm: {:.4f} || LR: {:.8f} || Batchtime: {:.4f} s || ETA: {}'
               .format(epoch, max_epoch, (iteration % epoch_size) + 1,
-              epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item(), lr, batch_time, str(datetime.timedelta(seconds=eta))))
+                      epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item(), lr, batch_time, str(datetime.timedelta(seconds=eta))))
 
     torch.save(net.state_dict(), save_folder + cfg['name'] + '_Final.pth')
     # torch.save(net.state_dict(), save_folder + 'Final_Retinaface.pth')
